df708f00a4ae74e9131a728f9cc2f4d1d35a0b68
mohethedreamy <mohethedreamy>
2018-04-21 23:44:17 +0100

    Changes the quotes box with its animation - static

--- static/js/script.js
+++ static/js/script.js
@@ -35,67 +35,50 @@ $(document).ready(function()
   );
 // =======================================================
 
-//  Quotes script
-
-  var $quotes = $('.quotes');
-  var $quote_text = '.text';
-  var $quote_author = '.author';
-  var $transition_time = 1200; // 1 second
-  var $time_between_slides = 100000; // 4 seconds
+// quotes animation
+  var $Quotes = $('.ArticleCopy'); // class or id of carousel slider
+  var $quote_text = 'blockquote div'; 
+  var $author = '.author'
+  var $transition_time = 1000; // 1 second
+  var $time_between_slides = 4000; // 4 seconds
 
   function quotes_texts(){
-    return $quotes.find($quote_text);
+    return $Quotes.find($quote_text);
   }
-  /* Adjust the size and position of quotes texts */
-  var $width = $quotes.width();
-  var $padding = $width * 8 / 100;
-  var $text_width = $width - ($padding * 2);
-  quotes_texts().width($text_width);
-  quotes_texts().css('left',$padding+'px');
-  var $index = $quotes.find($quote_text + '.active').index();
-  // retreive the highest text
-  var $max_height = 0;
-  var $texts = $('.quotes .text');
-  $texts.each(function($index){
-    var $height = $texts.eq($index).find('span').first().height();
-    if ($max_height < $height)
-      $max_height = $height;
-  });
-  //var $text_height = quotes_texts().eq($index).height();
-  $('.quote-box').height($max_height+40);
-   $texts.each(function($index){
-        console.log('max_height', $max_height + 40);
-        console.log('height', $texts.eq($index).find('span').first().height());
-        var $top = ($max_height + 40 - $texts.eq($index).find('span').first().height()) / 2;
-        console.log($top)
-        $texts.eq($index).css('top',$top+'px');
-    });
-  // console.log($max_height);
+  quotes_texts().fadeOut();
 
-//  quotes_texts().fadeOut();
+  var $authors = $Quotes.find($author);
+
   // set active classes
   quotes_texts().first().addClass('active');
+  quotes_texts().first().fadeIn($transition_time);
 
-  //quotes_texts().first().animate({left: "+=1000", opacity:0}, $transition_time);
+  $authors.first().addClass('active');
+  $authors.first().fadeIn($transition_time);
 
-  // auto scroll 
   $interval = setInterval(
     function(){
-      var $i = $quotes.find($quote_text + '.active').index();
+      var $i = $Quotes.find($quote_text + '.active').index();
 
-       quotes_texts().eq($i).animate({left:"+=1000", opacity:0}, $transition_time, function()
-       {
-         quotes_texts().eq($i).removeClass('active');
-         quotes_texts().eq($i).css('left','-=1000px');
-       }); // push away
-      if (quotes_texts().length == $i + 1) $i = -1; // loop to start
-      quotes_texts().eq($i + 1).css('left','-=1000px');
-      quotes_texts().eq($i + 1).animate({left:"+=1000", opacity:1}, $transition_time); // push in
-      quotes_texts().eq($i + 1).addClass('active');
+      quotes_texts().eq($i).removeClass('active');
+      $authors.eq($i).removeClass('active');
+
+      quotes_texts().eq($i).fadeOut($transition_time, function(){
+           if (quotes_texts().length == $i + 1) $i = -1; // loop to start
+
+            quotes_texts().eq($i + 1).fadeIn($transition_time, function() {
+            });
+            quotes_texts().eq($i + 1).addClass('active');
+              $authors.eq($i + 1).fadeIn($transition_time);
+              $authors.eq($i + 1).addClass('active');
+            
+      });
+          $authors.eq($i).fadeOut($transition_time);
+
+     
     }
     , $transition_time +  $time_between_slides 
   );
-
 } );
 
 
